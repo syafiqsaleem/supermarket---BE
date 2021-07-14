a451991cf207c056bdb3b914e29fbfaa6362bea3
syafiqsaleem <19syafiq89@gmail.com>
2021-07-14 21:22:05 +0800

    User Signup

---
+++ routers/userRoutes.js
@@ -0,0 +1,16 @@
+// const express = require("express");
+// const customerController = require('../controllers/customerController')
+// const router = express.Router();
+
+// app.post("/api/v1/customers/register", customerController.register);
+
+// module.exports = router;
+
+const express = require("express");
+const router = express.Router();
+
+const { signup } = require("../controllers/user");
+
+router.post("/signup", signup);
+
+module.exports = router;
