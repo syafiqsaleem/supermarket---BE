dbef364238cebee8d73dac73853daedf9be4faac
syafiqsaleem <19syafiq89@gmail.com>
2021-07-22 23:23:08 +0800

    Update sold products quantity

--- routers/order.js
+++ routers/order.js
@@ -6,12 +6,14 @@ const {
   addOrderToUserHistory,
 } = require("../controllers/userController");
 const { create } = require("../controllers/orderController");
+const { decreaseQuantity } = require("../controllers/productController");
 
 router.post(
   "/order/create/:userId",
   requireSignin,
   isAuth,
   addOrderToUserHistory,
+  decreaseQuantity,
   create
 );
 
